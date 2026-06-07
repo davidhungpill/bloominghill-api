33217b5832f70e7c1f57127aa9750d68b27db143
davidhungpil <davidhungpil@naver.com>
2026-06-07 23:39:27 +0900

    수정 - cors 설정 추가

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,26 @@
+import type { Core } from '@strapi/strapi';
 
-module.exports = [
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
   'strapi::errors',
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
-        'http://localhost:5173',
-        'https://kkotjae.or.kr',
-        'https://www.kkotjae.or.kr',
+        'https://bloominghill-front.davidhungpill.workers.dev',
       ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-]
+];
+
+export default config;

aee770b3404bcb05fb8cbb03c85dff5d322a7520
davidhungpil <davidhungpil@naver.com>
2026-06-07 22:51:28 +0900

    수정 - cors 추가

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,21 @@
-import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://kkotjae.or.kr',
+        'https://www.kkotjae.or.kr',
+      ],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
-
-export default config;
+]
